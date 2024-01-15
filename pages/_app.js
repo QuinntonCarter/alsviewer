44a241478e3dbd32f9789244716583507073187c
QuinntonCarter <qdevsz@gmail.com>
2024-01-14 19:37:52 -0700

    create maintenance branch

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,43 @@
 import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Container,
+  Image,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Layout>
-        <Component {...pageProps} />
+        <Container
+          justifyContent={"center"}
+          alignContent={"center"}
+          display={"flex"}
+          flexDirection={"column"}
+          height={"100vh"}
+        >
+          <Stack
+            background={"white"}
+            p={"10px"}
+            borderRadius={"6px"}
+            w={"auto"}
+          >
+            <Text as="h1" fontWeight={"bold"} size={"xxl"}>
+              Under maintenance
+            </Text>
+            <Image
+              src={
+                "https://media.tenor.com/v7Z6_aeZ1ocAAAAe/patrick-star-idk.png"
+              }
+              title="under construction"
+              alt="maintenance image holder"
+            />
+          </Stack>
+        </Container>
+        {/* <Component {...pageProps} /> */}
       </Layout>
     </ChakraProvider>
   );
