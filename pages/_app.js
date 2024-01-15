2522e352719d66177cf82c9f20433b202238ec98
QuinntonCarter <qdevsz@gmail.com>
2024-01-14 19:38:34 -0700

    Remove form

--- pages/_app.js
+++ pages/_app.js
@@ -11,34 +11,29 @@ import Layout from "../components/Layout";
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Layout>
-        <Container
-          justifyContent={"center"}
-          alignContent={"center"}
-          display={"flex"}
-          flexDirection={"column"}
-          height={"100vh"}
-        >
-          <Stack
-            background={"white"}
-            p={"10px"}
-            borderRadius={"6px"}
-            w={"auto"}
-          >
-            <Text as="h1" fontWeight={"bold"} size={"xxl"}>
-              Under maintenance
-            </Text>
-            <Image
-              src={
-                "https://media.tenor.com/v7Z6_aeZ1ocAAAAe/patrick-star-idk.png"
-              }
-              title="under construction"
-              alt="maintenance image holder"
-            />
-          </Stack>
-        </Container>
-        {/* <Component {...pageProps} /> */}
-      </Layout>
+      {/* <Layout> */}
+      <Container
+        justifyContent={"center"}
+        alignContent={"center"}
+        display={"flex"}
+        flexDirection={"column"}
+        height={"100vh"}
+      >
+        <Stack background={"white"} p={"10px"} borderRadius={"6px"} w={"auto"}>
+          <Text as="h1" fontWeight={"bold"} size={"xxl"}>
+            Under maintenance
+          </Text>
+          <Image
+            src={
+              "https://media.tenor.com/v7Z6_aeZ1ocAAAAe/patrick-star-idk.png"
+            }
+            title="under construction"
+            alt="maintenance image holder"
+          />
+        </Stack>
+      </Container>
+      {/* <Component {...pageProps} /> */}
+      {/* </Layout> */}
     </ChakraProvider>
   );
 }
