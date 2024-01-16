c432aaf16a1a9b9b6c0b99a4417963cf5c6fdfd5
QuinntonCarter <qdevsz@gmail.com>
2024-01-16 00:54:55 -0700

    commit for switch

--- components/Layout.js
+++ components/Layout.js
@@ -1,35 +1,27 @@
-import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import useSWR from "swr";
+import React, { useState } from "react";
 import { Container, Flex, Spinner } from "@chakra-ui/react";
 import usePlayer from "../hooks/usePlayer.js";
 import Header from "./Header.js";
 import Search from "./Search.js";
 import StatsViewer from "./StatsViewer.js";
 
-// const apexAPI = axios.create({
-//   baseURL: process.env.APEX_API_URL,
-//   // auth: process.env.APEX_API_ACCESS,
-// });
-
-// const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
 function Layout(props) {
-  // const { data, isLoading, error } = useSWR(
-  //   `https://api.mozambiquehe.re/bridge?auth=fKMNUxSguBohxghVtEds&player=malcolm hex&platform=PS4`,
-  //   fetcher
-  // );
+  // for inputs
+  // initially populated w player data for (none apex player) display UI
   const [platform, setPlatform] = useState("PS4");
   const [player, setPlayer] = useState("malcolm hex");
-  const { data, isLoading, isError } = usePlayer(player, platform);
-  console.log("initial player data", data);
+  const { foundStats, playerLegendData, isLoading, isError } = usePlayer(
+    player,
+    platform
+  );
+  console.log("initial player data", foundStats, playerLegendData);
 
   const [hoveredLegend, setHoveredLegend] = useState([]);
   const [selectedLegend, setSelectedLegend] = useState([]);
 
-  const [foundStats, setFoundStats] = useState({});
-  // const [error, setError] = useState(null);
-  const [playerLegendData, setPlayerLegendData] = useState([]);
+  // const [foundStats, setFoundStats] = useState({});
+  // const [playerLegendData, setPlayerLegendData] = useState([]);
+  const [error, setError] = useState(null);
 
   // const [loading, setLoading] = useState(false); ** don't need?
 
@@ -91,7 +83,7 @@ function Layout(props) {
   function resetData() {
     return setFoundStats({});
   }
-
+  // pulls associated entry data located at 1 index
   // const parsedPlayerLegendData = Object.entries(playerLegendData).filter(
   //   (entry) => entry[1].data
   // );
@@ -129,7 +121,7 @@ function Layout(props) {
   //   .catch((err) => showError(err.response.data.Error));
   // }, []);
 
-  if (isLoading || !data)
+  if (isLoading || !foundStats || !playerLegendData)
     return (
       <Container>
         <Flex direction={"column"}>
@@ -143,37 +135,36 @@ function Layout(props) {
 
   return (
     <Flex flexDirection={"column"} className="appContainer">
-      {/* {!foundStats.playerName ? ( */}
-      {/* <> */}
-      <Header />
-      <p> success placeholder: api data in console </p>
-      {/* <Search
+      {!foundStats.playerName ? (
+        <>
+          <Header />
+          <Search
             platform={platform}
             setPlatform={setPlatform}
             player={player}
             error={error}
-            loading={loading}
+            loading={isLoading}
             setError={setError}
             setPlayer={setPlayer}
             searchUser={searchUser}
             foundStats={foundStats}
-          /> */}
-      {/* </> */}
-      {/* ) : ( */}
-      {/* // <> placeholder test </> */}
-      {/* // <StatsViewer */}
-      {/* //   player={player}
-        //   foundStats={foundStats}
-        //   selectedLegend={selectedLegend}
-        //   setSelectedLegend={setSelectedLegend}
-        //   hoveredLegend={hoveredLegend}
-        //   setHoveredLegend={setHoveredLegend}
-        //   recentlyUsedLegend={foundStats.recentlyUsedLegend}
-        //   parsedLegendData={parsedPlayerLegendData}
-        //   playerLegendData={playerLegendData}
-        //   resetData={resetData}
-        // />
-      // )} */}
+          />
+        </>
+      ) : (
+        // <> placeholder test </>
+        <StatsViewer
+          player={player}
+          foundStats={foundStats}
+          selectedLegend={selectedLegend}
+          setSelectedLegend={setSelectedLegend}
+          hoveredLegend={hoveredLegend}
+          setHoveredLegend={setHoveredLegend}
+          recentlyUsedLegend={foundStats.recentlyUsedLegend}
+          // parsedLegendData={playerLegendData}
+          playerLegendData={playerLegendData}
+          resetData={resetData}
+        />
+      )}
       {props.children}
     </Flex>
   );
--- hooks/usePlayer.js
+++ hooks/usePlayer.js
@@ -1,28 +1,50 @@
+import { useState, useEffect } from "react";
 import useSWR from "swr";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const usePlayer = (player, platform) => {
+  const [foundStats, setFoundStats] = useState({});
+  const [playerLegendData, setPlayerLegendData] = useState([]);
   const { data, error, isLoading } = useSWR(
     `https://api.mozambiquehe.re/bridge?auth=${process.env.APEX_API_ACCESS}&player=${player}&platform=${platform}`,
     fetcher
   );
 
+  useEffect(() => {
+    // if data
+    if (data) {
+      let parsedPlayerLegendData = Object.entries(data.legends.all).filter(
+        (entry) => entry[1].data
+      );
+      // set response data to state on successful fetch
+      setFoundStats({
+        playerName: data.global.name,
+        playerLvl: data.global.level,
+        totalData: data.total,
+        banStatus: [data.global.bans],
+        currentArenaRank: [data.global.arena],
+        currentBrRank: [data.global.rank],
+        recentlyUsedLegend: data.legends.selected,
+      });
+      setPlayerLegendData(parsedPlayerLegendData);
+      console.log("got data?", data, "parsed", parsedPlayerLegendData);
+    }
+  }, [data]);
+
   if (isLoading) return <p> Loading... </p>;
-  if (error) return <p> error retrieving data </p>;
+  if (error)
+    return (
+      <p>
+        error retrieving data
+        {console.log("error retrieving player data", error)}
+      </p>
+    );
   return {
-    // swr response of only needed data
-    data: {
-      playerName: data.global.name,
-      playerLvl: data.global.level,
-      totalData: data.total,
-      banStatus: [data.global.bans],
-      currentArenaRank: [data.global.arena],
-      currentBrRank: [data.global.rank],
-      recentlyUsedLegend: data.legends.selected,
-      playerLegendData: data.legends.all,
-    },
-    // swr
+    // SWR response of only needed data
+    foundStats,
+    playerLegendData,
+    // SWR
     isLoading,
     isError: error,
   };
